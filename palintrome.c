6d8875b2dce7411923b16bfba7b7d72d03b8effc
jayesh-warhadi <www.jayeshwarhadi0308@gmail.com>
2024-07-31 21:54:35 +0530

    added palintrome project

---
+++ palintrome.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int reverse(int n){
+    
+}
+
+int main(){
+    int num;
+    printf("Enter the number : ");
+    scanf("%d", &num);
+
+
+    printf("%d",num);
+    return 0;
+}
