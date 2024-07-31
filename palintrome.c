2f5b44431356846e96fc1d8f75dffaccc3a0bf12
jayesh-warhadi <www.jayeshwarhadi0308@gmail.com>
2024-07-31 22:11:11 +0530

    new update code

--- palintrome.c
+++ palintrome.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
 
 int reverse(int n){
-    
+    int m = n;
+    while(n!=0){
+        int r = n % 10;
+        n = n/10;
+    }
 }
 
 int main(){
     int num;
     printf("Enter the number : ");
     scanf("%d", &num);
+    reverse(num);
 
-
-    printf("%d",num);
+    // printf("%d",num);
     return 0;
 }
