2554c076b244ac7cb1a569b2125200dfe333e925
jayesh-warhadi <www.jayeshwarhadi0308@gmail.com>
2024-08-06 20:41:47 +0530

    SPS

--- test.c
+++ test.c
@@ -1,114 +1,133 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+// Main focus is to use "random" statistically and extensively
 
-int reverse(int n)
+int randint(int x, int y)
 {
-    int s = 0;
-    while (n != 0)
+    srand(time(0));
+    // random seed
+    int a = (rand() % (y - x + 1)) + x;
+    int b = (rand() % (y - x + 1)) + x;
+    int c = (rand() % (y - x + 1)) + x;
+    return (((a * b * c) % (y - x + 1)) + x);
+}
+
+char *outcome(int x)
+{
+    if (x == 1)
     {
-        int r = n % 10;
-        n = n / 10;
-        s = s * 10 + r;
+        return "STONE";
     }
-    return s;
+    else if (x == 2)
+    {
+        return "PAPER";
+    }
+    else if (x == 3)
+    {
+        return "SCISSORS";
+    }
 }
 
 int main()
 {
-    int run = 1;
-    while (run == 1)
+    printf("********************\n\n STONE PAPER SCISSORS \n\n  (jayesh warhadi)  \n\n********************\n");
+    int score = 0;
+    int streak = 0;
+    int userguess;
+    while (1)
     {
-        printf("********************\n\n  PALINDROME TEST  \n\n  (jayesh warhadi)  \n\n********************\n");
-        
-        int num;
-        int result;
-        while (1) {
-            printf("Enter the number: ");
-            result = scanf("%d", &num);
-            if (result == 1) {
-                break; // valid input
-            } else {
-                printf("Invalid input. Please enter a valid number.\n");
-                // Clear the input buffer
-                while(getchar() != '\n');
+        int compguess = randint(1, 3);
+        printf("********************\n>\n> STONE - 1\n>\n> PAPER - 2\n>\n> SCISSORS - 3\n>\n********************\n");
+        while (1)
+        {
+            printf("Enter input : ");
+            int res = scanf("%d", &userguess);
+            if (res == 1 && userguess <= 3 && userguess >= 1)
+            {
+                break;
+            }
+            else
+            {
+                printf("Invalid input : Try again! (1~3)\n");
+                while (getchar() != '\n')
+                    ;
             }
         }
-
-        int rev = reverse(num);
-        if (rev == num)
+        if (userguess == compguess)
         {
-            printf("\nThe number is a palindrome\n");
+            printf("Round Tie\n");
+            printf("Your choice : %s || computer choice %s\n", outcome(userguess), outcome(compguess));
+            score = score + streak * 5;
+            printf("# Score => %d (%d - streak bonus) #\n", score, streak * 5);
+            streak++;
+        }
+        else if (userguess == 1 && compguess == 2)
+        {
+            printf("Round Lost\n");
+            printf("Your choice : %s || computer choice %s\n", outcome(userguess), outcome(compguess));
+            int prevscore = score;
+            score = score * 0.1 + streak * 5;
+            printf("# Score => %d (%d - punish) #\n", score, prevscore-score);
+            printf("# Streak reset #\n");
+            streak = 0;
+        }
+        else if (userguess == 1 && compguess == 3)
+        {
+            printf("Round Won\n");
+            printf("Your choice : %s || computer choice %s\n", outcome(userguess), outcome(compguess));
+            score = score + 100 + streak * 20;
+            printf("# Score => %d (%d - streak bonus) #\n", score, streak * 20);
+            streak++;
+        }
+        else if (userguess == 2 && compguess == 1)
+        {
+            printf("Round Won\n");
+            printf("Your choice : %s || computer choice %s\n", outcome(userguess), outcome(compguess));
+            score = score + 100 + streak * 20;
+            printf("# Score => %d (%d - streak bonus) #\n", score, streak * 20);
+            streak++;
+        }
+        else if (userguess == 2 && compguess == 3)
+        {
+            printf("Round Lost\n");
+            printf("Your choice : %s || computer choice %s\n", outcome(userguess), outcome(compguess));
+            int prevscore = score;
+            score = score * 0.1 + streak * 5;
+            printf("# Score => %d (%d - punish) #\n", score, prevscore-score);
+            printf("# Streak reset #\n");
+            streak = 0;
+        }
+        else if (userguess == 3 && compguess == 1)
+        {
+            printf("Round Lost\n");
+            printf("Your choice : %s || computer choice %s\n", outcome(userguess), outcome(compguess));
+            int prevscore = score;
+            score = score * 0.1 + streak * 5;
+            printf("# Score => %d (%d - punish) #\n", score, prevscore-score);
+            printf("# Streak reset #\n");
+            streak = 0;
+        }
+        else if (userguess == 3 && compguess == 2)
+        {
+            printf("Round Won\n");
+            printf("Your choice : %s || computer choice %s\n", outcome(userguess), outcome(compguess));
+            score = score + 100 + streak * 20;
+            printf("# Score => %d (%d - streak bonus) #\n", score, streak * 20);
+            streak++;
+        }
+        printf("Press Enter to continue and any key to exit\n");
+        while (getchar() != '\n')
+            ;
+        if (getchar() == '\n')
+        {
+            continue;
         }
         else
         {
-            printf("\nThe number is not a palindrome\n");
-        }
-
-        int res;
-        while (1) {
-            printf("Press 0 to continue, 1 to exit: ");
-            res = scanf("%d", &run);
-            if (res == 1 && (run == 0 || run == 1)) {
-                break; // valid input
-            } else {
-                printf("Invalid input. Please enter 0 or 1.\n");
-                // Clear the input buffer
-                while(getchar() != '\n');
-            }
+            printf("********************\n\nCurrent Score is %d\nCurrent Streak is %d\n\n********************",score,streak);
+            return 0;
         }
     }
     return 0;
 }
-
-// #include <stdio.h>
-
-// int reverse(int n)
-// {
-//     int m = n;
-//     int s = 0;
-//     while (n != 0)
-//     {
-//         int r = n % 10;
-//         n = n / 10;
-//         s = s * 10 + r;
-//     }
-//     return s;
-// }
-
-// int main()
-// {
-//     int run = 1;
-//     while (run == 1)
-//     {
-//         printf("********************\n\n  PALINTROME TEST  \n\n  (jayesh warhadi)  \n\n********************\n");
-//         int num;
-//         printf("Enter the number : ");
-//         scanf("%d", &num);
-//         int rev = reverse(num);
-//         if (rev == num)
-//         {
-//             printf("\nThe number is a palintrome\n");
-//         }
-//         else
-//         {
-//             printf("\nThe number is not a palintrome\n");
-//         }
-//         printf("press 0 to continue\n");
-//         int res = scanf("%d" , &run);
-//         while (res != 1)
-//         {
-//             printf("Invalid Input (Try Again!)\n");
-//             res = scanf("%d", &run);
-//         }
-//         if (run == 0)
-//         {
-//             run = 1;
-//             continue;
-//         }
-//         else
-//         {
-//             run = 0;
-//             break;
-//         }
-//     }
-//     return 0;
-// }
